5b7d923f50a714a19bd3b35ee16efccbc112acc8
sillypoise <somewhatjose@gmail.com>
2024-10-28 21:07:58 -0500

    add nix

---
+++ roles/base/files/users/sillypoise/nix/home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable Home Manager
+  home.stateVersion = "23.05"; # Change this to the appropriate version if needed
+
+  # Package Management
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    fzf
+    bat
+    broot
+    fd
+    eza
+    gnused
+    sd
+    lazygit
+    lazydocker
+    jq
+    tree-sitter
+    starship
+    btop
+    gh
+    tailscale
+    git
+    zellij
+    sqlite-utils
+    datasette
+    rustup
+    rsync
+    awscli2
+    aws-vault
+    delta
+    volta
+    zoxide
+  ];
+}
+
